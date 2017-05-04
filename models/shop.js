14dbf37b2c43889ebaa774f5b0153e88a4e67dc9
Ankush Sharma <ankushs@smartdatainc.net>
2017-05-04 14:42:41 +0530

    Swagger implementation

--- models/shop.js
+++ models/shop.js
@@ -2,98 +2,106 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var shopSchema = new mongoose.Schema({
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
-  },
-  name: String,
-  address: String,
-  city: String,
-  state: String,
-  zip: String,
-  latLong: {
-    type: [Number], // longitude first and latitude after
-    index: '2dsphere'
-  },
-  phone: Number,
-  license_number: Number,
-  created_date: {
-    type: Date,
-    default: Date.now()
-  },
-  modified_date: {
-    type: Date,
-    default: Date.now()
-  },
-  payment_methods: [{
-    method: String,
-    card_type: String,
-    is_primary: Boolean,
-    card_id: String,
-    first_name: String,
-    last_name: String,
-    card_no: Number,
-    status: Boolean,
-    created_date: {
-      type: Date,
-      default: Date.now()
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
     },
-    modified_date: {
-      type: Date,
-      default: Date.now()
-    }
-  }],
-  ratings: [{
-    rated_by: {
-      type: Schema.Types.ObjectId,
-      ref: 'users'
-    },
-    score: Number,
-    comments: String
-  }],
-  chairs: [{
     name: String,
-    availability: {
-      type: String,
-      enum: ["booked", "available"]
+    address: String,
+    city: String,
+    state: String,
+    zip: String,
+    latLong: {
+        type: [Number], // longitude first and latitude after
+        index: '2dsphere'
     },
-    type:{
-      type: String,
-      enum: ["weekly", "monthly","percentage"]
-    },
-    amount:Number,
-    shop_percentage: Number,
-    barber_percentage: Number,
-    booking_start:Date,
-    booking_end:Date,
-    barber_id:{
-      type: Schema.Types.ObjectId,
-      ref: 'users'
-    },
-    barber_name:String,
-    isActive:{
-      type:Boolean,
-      default:true
-    },
+    phone: Number,
+    license_number: String,
     created_date: {
-      type: Date,
-      default: Date.now()
+        type: Date,
+        default: Date.now()
     },
     modified_date: {
-      type: Date,
-      default: Date.now()
-    }
-  }],
-  image: String,
-  gallery: [{
-    name: {
-      type: String
+        type: Date,
+        default: Date.now()
     },
-    created_date: {
-      type: Date,
-      default: Date.now()
-    }
-  }]
+    payment_methods: [
+        {
+            method: String,
+            card_type: String,
+            is_primary: Boolean,
+            card_id: String,
+            first_name: String,
+            last_name: String,
+            card_no: Number,
+            status: Boolean,
+            created_date: {
+                type: Date,
+                default: Date.now()
+            },
+            modified_date: {
+                type: Date,
+                default: Date.now()
+            }
+        }
+    ],
+    ratings: [
+        {
+            rated_by: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            score: Number,
+            comments: String
+        }
+    ],
+    chairs: [
+        {
+            name: String,
+            availability: {
+                type: String,
+                enum: ["booked", "available"]
+            },
+            type: {
+                type: String,
+                enum: ["weekly", "monthly", "percentage"]
+            },
+            amount: Number,
+            shop_percentage: Number,
+            barber_percentage: Number,
+            booking_start: Date,
+            booking_end: Date,
+            barber_id: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            barber_name: String,
+            isActive: {
+                type: Boolean,
+                default: true
+            },
+            created_date: {
+                type: Date,
+                default: Date.now()
+            },
+            modified_date: {
+                type: Date,
+                default: Date.now()
+            }
+        }
+    ],
+    image: String,
+    gallery: [
+        {
+            name: {
+                type: String
+            },
+            created_date: {
+                type: Date,
+                default: Date.now()
+            }
+        }
+    ]
 });
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -115,7 +115,103 @@ var upload = multer({storage: storage})
 *               description: "date in UTC when token expires"
 *         400:
 *           description: "Invalid username/password supplied"
-*           
+*   /shops:
+*     get:
+*       tags:
+*       - "shop"
+*       summary: "Shops Listing"
+*       description: "List all the shops based on radius search"
+*       operationId: "shops"
+*       produces:
+*       - "application/json"
+*       parameters:
+*       - in: "header"
+*         name: "device_latitude"
+*         description: "Device latitude"
+*         required: true
+*         type: string
+*         format: string
+*         default: "30.538994"
+*       - in: "header"
+*         name: "device_longitude"
+*         description: "Device Longitude"
+*         required: true
+*         type: string
+*         format: string
+*         default: "75.955033"
+*       responses:
+*         200:
+*           description: "successful operation"
+*           schema:
+*             type: "string"
+*           headers:
+*             X-Rate-Limit:
+*               type: "integer"
+*               format: "int32"
+*               description: "calls per hour allowed by the user"
+*             X-Expires-After:
+*               type: "string"
+*               format: "date-time"
+*               description: "date in UTC when token expires"
+*         400:
+*           description: "Invalid request"
+*     put:
+*       tags:
+*       - "shop"
+*       summary: "Shop Update"
+*       description: "Update shop details"
+*       operationId: "shopUpdate"
+*       produces:
+*       - "application/json"
+*       parameters:
+*       - in: "header"
+*         name: "device_latitude"
+*         description: "Device latitude"
+*         required: true
+*         type: string
+*         format: string
+*         default: "30.538994"
+*       - in: "header"
+*         name: "device_longitude"
+*         description: "Device Longitude"
+*         required: true
+*         type: string
+*         format: string
+*         default: "75.955033"
+*       - in: "header"
+*         name: "user_id"
+*         description: "Logged in user's id"
+*         required: true
+*         type: string
+*         format: string
+*         default: "59072e9a2fe2cb0f2cafa060"
+*       - in: "body"
+*         name: "body"
+*         description: "Created shop object"
+*         required: true
+*         schema:
+*           $ref: "#/definitions/Shop"
+*       - in: "body"
+*         name: "image"
+*         description: "Upload shop image"
+*         required: false
+*         type: file
+*       responses:
+*         200:
+*           description: "successful operation"
+*           schema:
+*             type: "string"
+*           headers:
+*             X-Rate-Limit:
+*               type: "integer"
+*               format: "int32"
+*               description: "calls per hour allowed by the user"
+*             X-Expires-After:
+*               type: "string"
+*               format: "date-time"
+*               description: "date in UTC when token expires"
+*         400:
+*           description: "Invalid request"
 * definitions:
 *    User:
 *     type: "object"
@@ -126,38 +222,77 @@ var upload = multer({storage: storage})
 *         type: "string"
 *       email:
 *         type: "string"
+*         default: "ankushs@smartdatainc.net"
 *       password:
 *         type: "string"
 *       mobile_number:
 *         type: "string"
+*         default: "9876543210"
 *    Userlogin:
 *     type: "object"
 *     properties:
 *       email:
 *         type: "string"
+*         default: "ankushs@smartdatainc.net"
 *       password:
 *         type: "string"
+*         default: "123456"
+*    Shop:
+*     type: "object"
+*     properties:
+*       _id:
+*         type: "string"
+*         default: "5901e07846c94a225018d5cc"
+*       name:
+*         type: "string"
+*         default: "Pop's Barber Shop"
+*       license_number:
+*         type: "string"
+*         default: "123456ABCDEF"
+*       address:
+*         type: "string"
+*         default: "Street 123"
+*       state:
+*         type: "string"
+*         default: "Punjab"
+*       city:
+*         type: "string"
+*         default: "Mohali"
+*       zip:
+*         type: "string"
+*         default: "160071"  
+*       image:
+*         type: "file"
 */
-app.post('/contact', contactController.contactPost);
-app.post('/api/v1/account', userController.ensureAuthenticated, userController.accountPut);
-app.delete('/api/v1/account', userController.ensureAuthenticated, userController.accountDelete);
+//Users
 app.post('/api/v1/signup', userController.signupPost);
 app.post('/api/v1/login', userController.loginPost);
 app.post('/api/v1/forgot', userController.forgotPost);
-app.post('/reset/:token', userController.resetPost);
+app.post('/api/v1/account', userController.ensureAuthenticated, userController.accountPut);
+app.delete('/api/v1/account', userController.ensureAuthenticated, userController.accountDelete);
 app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
 app.post('/auth/facebook', userController.authFacebook);
 app.get('/auth/facebook/callback', userController.authFacebookCallback);
 app.post('/auth/google', userController.authGoogle);
 app.get('/auth/google/callback', userController.authGoogleCallback);
 
+//
+app.post('/reset/:token', userController.resetPost);
+
+//Shops
+app.get('/api/v1/shops', customerController.allShops);
+app.put('/api/v1/shops',upload.any() ,shopController.editShop);
+
 app.post('/api/v1/addChair',userController.addChair)
 app.post('/api/v1/removeChair', userController.removeChair);
-app.get('/api/v1/shops', customerController.allShops);
+
 app.post('/api/v1/takeAppointment', appointmentController.takeAppointment);
 app.post('/api/v1/requestChair', chairRequestController.requestChair);
 app.post('/api/v1/bookChair', chairRequestController.bookChair);
-app.post('/api/v1/shops',upload.any() ,shopController.editShop);
+
 app.get('/api/v1/getUserType',userController.ensureAuthenticated, userController.getUserType);
-app.post('/api/v1/customerAppointments',appointmentController.customerAppointments)
+app.post('/api/v1/customerAppointments',appointmentController.customerAppointments);
+
+
+app.post('/contact', contactController.contactPost);
 }

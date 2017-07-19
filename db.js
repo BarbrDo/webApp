e9f1444ce859bcabe13bffbda9a4d037789d23c8
Ankush Sharma <ankushs@smartdatainc.net>
2017-07-19 12:04:22 +0530

    updated gitignore

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/barberdo');
+//mongoose.connect('mongodb://localhost/barberdo');
+mongoose.connect('mongodb://barbrdo:barbrdo2780@52.39.212.226:27017/barbrdo',{auth:{authdb:"barbrdo"}});
 mongoose.connection.on('error', function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);

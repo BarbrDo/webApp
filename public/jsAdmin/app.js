c6daa3154c8ee93275f99934fe8a95bab46a8b15
Rahul Dadwal <dadwal.rahul20@gmail.com>
2017-07-24 18:34:45 +0530

    Resolving conflicts

--- public/jsAdmin/app.js
+++ public/jsAdmin/app.js
@@ -477,13 +477,8 @@
             templateUrl: "/modalsAdmin/adminHTml/views/reports.html"
         })
 
-<<<<<<< HEAD
         .state('add_services', {
             resolve: {
-=======
-        .state('cms', {
-                resolve: {
->>>>>>> 91bd306da8ecb286657a1e13c3068b08c302d47a
                     mess: function($localStorage,$q,$state) {
                         var deferred = $q.defer();
                         if ($localStorage.loggedIn!= true) {
@@ -495,18 +490,13 @@
                         }
                     }
                 },
-<<<<<<< HEAD
+
             url: '/add_services',
             controller: "AdminCtrl",
             templateUrl: "/modalsAdmin/adminHTml/views/add_services.html"
         })
 
-=======
-                url: '/cms/add',
-                controller: "AdminCtrl",
-                templateUrl: 'templatesAdmin/cmsAdd.html'
-            })
->>>>>>> 91bd306da8ecb286657a1e13c3068b08c302d47a
+
 
     });
 

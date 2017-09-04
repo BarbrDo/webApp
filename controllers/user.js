20d27abce453f8fd97481f4a31c8e2a73bb0479f
Hussain Mohammed <hussain.mohammed@Hussain.local>
2017-09-04 17:13:46 +0530

    saveinfo

--- controllers/user.js
+++ controllers/user.js
@@ -370,7 +370,7 @@ exports.signupPost = function(req, res, next) {
       }
     },
     function(saveData, done) {
-      User(saveinfo).save(saveinfo,function(err, data) {
+      User(saveData).save(saveData,function(err, data) {
         if (err) {
           return res.status(400).send({
             msg: constantObj.messages.errorInSave,
